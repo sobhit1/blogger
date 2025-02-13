40508f2d7455ea389a2700fa40709eda4a530609
sobhit1 <sobhitraghav1092@gmail.com>
2025-02-13 05:44:18 +0530

    fixed signup

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -31,7 +31,13 @@ router.post("/signup",
       }
       const newUser = new User({ name, email, password: password });
       await newUser.save();
-      return res.redirect("/signin");
+      const token = await User.matchPasswordAndGenerateToken(email, password);
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: 'Strict',
+        maxAge: 24 * 60 * 60 * 1000
+      }).redirect("/");
     }
     catch (err) {
       console.error("Error during sign up:", err.message);
@@ -52,14 +58,6 @@ router.post("/signin",
     }
     const { email, password } = req.body;
     try {
-      const user = await User.findOne({ email });
-      if (!user) {
-        return res.render("signin", { error: "User not found" });
-      }
-      const isPasswordValid = await bcrypt.compare(password, user.password);
-      if (!isPasswordValid) {
-        return res.render("signin", { error: "Invalid Password" });
-      }
       const token = await User.matchPasswordAndGenerateToken(email, password);
       if (!token) {
         return res.render("signin", { error: "Invalid Password" });
